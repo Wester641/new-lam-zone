f8abdbb3866ccf7b1e71883d33e329d734f0a36e
zafarzhon <zafarzhon77@gmail.com>
2025-05-14 23:21:59 +0600

    fixed styles in black friday and bottom widgets

--- src/components/features/black_friday/BlackFridayWidget.tsx
+++ src/components/features/black_friday/BlackFridayWidget.tsx
@@ -1,36 +1,38 @@
 import Box from '@mui/material/Box';
 import styles from './BlackFridayWidget.module.scss';
-import { AppBar, Button, Container, Typography } from '@mui/material';
+import { Button, Container, Grid } from '@mui/material';
 import { ArrowForward, Close } from '@mui/icons-material';
 
 export default function BlackFridayWidget() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar className={styles.blackFridayWidget}>
-        <Container maxWidth="lg" className={styles.container}>
-          <Typography className={styles.blackTypegraphy} noWrap component="div">
+    <Box sx={{ flexGrow: 1 }} className={styles.blackFridayWidget}>
+      <Container className={styles.container}>
+        <Grid container spacing={0}>
+          <Grid size={{ xs: 12, sm: 5, md: 4 }} className={styles.gridItems}>
             <span className={styles.blackText}>Black </span>
             <span className={styles.fridayText}>Friday</span>
-          </Typography>{' '}
-          <Typography className={styles.blackTypegraphy} noWrap component="div">
+          </Grid>
+          <Grid
+            size={{ xs: 12, sm: 5, md: 4 }}
+            justifyContent="center"
+            className={styles.gridItems}
+          >
             <span className={styles.firstText}>Up to </span>
             <span className={styles.secondText}> 59%</span>
             <span className={styles.thirdText}>OFF</span>
-          </Typography>
-          <Typography className={styles.blackTypegraphy} noWrap component="div">
+          </Grid>
+          <Grid size={{ sm: 5, md: 4 }} className={styles.gridItems}>
             <Button
               variant="outlined"
               className={styles.shopNow}
               endIcon={<ArrowForward />}
             >
-              Shop Now
+              <span>Shop Now</span>
             </Button>
-          </Typography>
-          <Typography className={styles.blackTypegraphy} noWrap component="div">
-            <Close className={styles.closeButton} />
-          </Typography>
-        </Container>
-      </AppBar>
+          </Grid>
+        </Grid>
+      </Container>
+      <Close className={styles.closeButton} />
     </Box>
   );
 }
--- src/components/features/bottom_menu/BottomMenuWidget.tsx
+++ src/components/features/bottom_menu/BottomMenuWidget.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import styles from './BottomMenuWidget.module.scss';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import SelectedMenu from '../../common/select/SelectedMenu';
 import HeadsetIcon from '@mui/icons-material/Headset';
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
@@ -10,32 +10,43 @@ import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 
 export default function BottomMenuWidget() {
   return (
-    <Box className={styles.bottomMenuWidget}>
+    <Box sx={{ flexGrow: 1 }} className={styles.bottomMenuWidget}>
       <Container maxWidth="lg" className={styles.container}>
-        <SelectedMenu />
-        <Box className={styles.boxContainer}>
-          <div className={styles.box}>
-            <PinDropIcon />
-            <span>Track Order</span>
-          </div>
-          <div className={styles.box}>
-            <CompareArrowsIcon />
-            <span>Compare</span>
-          </div>
-          <div className={styles.box}>
-            <HeadsetIcon />
-            <span>Customer Support</span>
-          </div>
-          <div className={styles.box}>
-            <ErrorIcon />
-            <span>Need Help</span>
-          </div>
-        </Box>
-
-        <Box className={styles.boxContainer}>
-          <PhoneInTalkIcon />
-          <span className={styles.callUsNow}>+996 500 37 00 02</span>
-        </Box>
+        <Grid
+          container
+          spacing={{ xs: 0, sm: 0, md: 2 }}
+          className={styles.grid}
+        >
+          <Grid className={styles.gridItems} size={{ sm: 0, md: 2 }}>
+            <SelectedMenu />
+          </Grid>
+          <Grid className={styles.gridItems} size={{ xs: 12, sm: 12, md: 8 }}>
+            <Box className={styles.boxContainer}>
+              <div className={styles.box}>
+                <PinDropIcon />
+                <span>Track Order</span>
+              </div>
+              <div className={styles.box}>
+                <CompareArrowsIcon />
+                <span>Compare</span>
+              </div>
+              <div className={styles.box}>
+                <HeadsetIcon />
+                <span>Customer Support</span>
+              </div>
+              <div className={styles.box}>
+                <ErrorIcon />
+                <span>Need Help</span>
+              </div>
+            </Box>
+          </Grid>
+          <Grid className={styles.gridItems} size={{ md: 2 }}>
+            <Box>
+              <PhoneInTalkIcon />
+              <span className={styles.callUsNow}>+996 500 37 00 02</span>
+            </Box>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
