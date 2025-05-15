2e91557e76ce79e390c3645d21fa206b68809aec
zafarzhon <zafarzhon77@gmail.com>
2025-05-15 10:45:11 +0600

    fixed socialItemsWidget

--- src/components/features/social_items_header/SocialItemsWidget.tsx
+++ src/components/features/social_items_header/SocialItemsWidget.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import styles from './SocialItemsWidget.module.scss';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import {
   Facebook,
   Instagram,
@@ -12,22 +12,24 @@ import {
 export default function SocialItemsWidget() {
   return (
     <Box sx={{ flexGrow: 1 }} className={styles.socialItemsWidget}>
-      <Container maxWidth="lg" className={styles.container}>
-        <Box className={styles.boxText}>
-          <span>Welcome to L&M zone online store, enjoy your drive. </span>
-        </Box>
-        <Box className={styles.boxText}>
-          <div>
-            <span>Follow Us:</span>
-          </div>
-          <div className={styles.social}>
-            <Telegram></Telegram>
-            <WhatsApp></WhatsApp>
-            <Instagram></Instagram>
-            <Facebook></Facebook>
-            <YouTube></YouTube>
-          </div>
-        </Box>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid size={{ xs: 12, sm: 12, md: 6 }} className={styles.gridItem}>
+            <span>Welcome to L&M zone online store, enjoy your drive. </span>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 12, md: 6 }} className={styles.gridItem}>
+            <div>
+              <span>Follow Us:</span>
+            </div>
+            <div className={styles.social}>
+              <Telegram></Telegram>
+              <WhatsApp></WhatsApp>
+              <Instagram></Instagram>
+              <Facebook></Facebook>
+              <YouTube></YouTube>
+            </div>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
