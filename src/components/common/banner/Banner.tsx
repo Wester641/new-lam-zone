6b6643fbdcb1007d7d56dde6c440651bb33616de
zafarzhon <zafarzhon77@gmail.com>
2025-05-19 17:52:05 +0600

    finish [fix]

--- src/components/common/banner/Banner.tsx
+++ src/components/common/banner/Banner.tsx
@@ -37,10 +37,10 @@ export default function Banner() {
             </Box>
             <Box className={styles.boxContainer}>
               <div className={styles.container}>
-                <span className={styles.textHeadphones}>
-                  Sony Headphone <br /> Ultra 21
+                <span className={styles.text}>
+                  Sony Headphones <br /> Ultra 21
                 </span>
-                <span className={styles.price}>$250 USD</span>
+                <span className={styles.price}>$250</span>
                 <ButtonComponent />
               </div>
               <div className={styles.img}>
