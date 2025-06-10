eccbb160eb97dec9c68a07d202fdd22f88027c81
zafarzhon <zafarzhon77@gmail.com>
2025-06-10 16:13:32 +0600

    [added] skeleton

--- src/pages/catalogue/CataloguePage.tsx
+++ src/pages/catalogue/CataloguePage.tsx
@@ -1,6 +1,27 @@
-import UnderConstruction from '../../components/common/developing/UnderConstruction';
+import { Box, Container, Grid } from '@mui/material';
+import HeaderLayout from '../../components/layout/header/HeaderLayout';
+import { MobileBottomNavigation } from '../../components/common/header_components/mobile_navigation/MobileBottomNavigation';
+import FooterLayout from '../../components/layout/footer/FooterLayout';
+import styles from './CataloguePage.module.scss';
+
+const categories = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3];
 
 function CataloguePage() {
-  return <UnderConstruction />;
+  return (
+    <Box className={styles.homePage}>
+      <HeaderLayout />
+      <MobileBottomNavigation />
+      <Container className={styles.grid}>
+        <Grid container spacing={2}>
+          {categories.map(category => (
+            <Grid className={styles.gridItem} size={{ xs: 6, md: 3 }}>
+              {category}
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <FooterLayout />
+    </Box>
+  );
 }
 export default CataloguePage;
