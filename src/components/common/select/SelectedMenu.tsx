883bf085abf3ad25ea987c6ddc3ba53aa0c31f66
ozodbek <ozodbek200017@gmail.com>
2025-08-05 14:15:17 +0600

    [Add] category dropdown window

--- src/components/common/select/SelectedMenu.tsx
+++ src/components/common/select/SelectedMenu.tsx
@@ -1,40 +1,107 @@
-import Typography from '@mui/material/Typography';
-import Popper from '@mui/material/Popper';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import {
-  usePopupState,
-  bindHover,
-  bindPopper,
-} from 'material-ui-popup-state/hooks';
-import styles from './SelectedMenu.module.scss';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { useState, useRef } from "react";
+import Button from "@mui/material/Button";
+import Popper from "@mui/material/Popper";
+import Paper from "@mui/material/Paper";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-const SelectedMenu = () => {
-  const popupState = usePopupState({
-    variant: 'popper',
-    popupId: 'demoPopper',
-  });
+import styles from "./SelectedMenu.module.scss";
+
+const BurgerMenu = () => {
+  const [open, setOpen] = useState(false);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+  const anchorRef = useRef<HTMLButtonElement | null>(null);
+  const submenuRef = useRef<HTMLLIElement | null>(null);
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: MouseEvent | TouchEvent) => {
+    if (anchorRef.current) {
+      return;
+    }
+    setOpen(false);
+    setSubmenuOpen(false);
+  };
+
+  const handleSubmenuToggle = () => {
+    setSubmenuOpen((prev) => !prev);
+  };
+
+  const [items, setItems] = useState([
+    "All",
+    "iPhone",
+    " Sansung",
+    "Realme",
+    "Xiaomi",
+    "Oppo",
+    "Vivo",
+    "OnePlus",
+    "Huawei",
+    "Infinix",
+    "Tecno",
+  ]);
+
+  const [items2, setItems2] = useState([
+    " Computer & Laptop",
+    " Computer Accessories",
+    " SmartPhone",
+    "Headphone",
+    "  Mobile Accessories",
+    "  Gaming Console",
+    " Camera & Photo",
+    " TV & Homes Appliances",
+    " Watchs & Accessories",
+    " GPS & Navigation",
+    " Warable Technology",
+  ]);
 
   return (
-    <div className={styles.main}>
-      <Button className={styles.button} {...bindHover(popupState)}>
+    <div>
+      <Button ref={anchorRef} onClick={handleToggle} className={styles.button}>
         Categories
-        <KeyboardArrowDownIcon sx={{ fontSize: 20, marginLeft: '5px' }} />
+        <KeyboardArrowDownIcon sx={{ fontSize: 20, marginLeft: "5px" }} />
       </Button>
       <Popper
-        className={styles.popper}
-        {...bindPopper(popupState)}
-        placement="bottom"
+        open={open}
+        anchorEl={anchorRef.current}
+        placement="bottom-start"
+        modifiers={[{ name: "offset", options: { offset: [0, 10] } }]}
+      >
+        <ClickAwayListener onClickAway={handleClose}>
+          <Paper>
+            <MenuList autoFocusItem={open}>
+              {items2.map((item, index) => (
+                <div key={index}>
+                  <MenuItem ref={submenuRef} onClick={handleSubmenuToggle}>
+                    {item}
+                  </MenuItem>
+                </div>
+              ))}
+            </MenuList>
+          </Paper>
+        </ClickAwayListener>
+      </Popper>
+      <Popper
+        open={submenuOpen}
+        anchorEl={submenuRef.current}
+        placement="right-start"
+        modifiers={[{ name: "offset", options: { offset: [0, 0] } }]}
       >
         <Paper>
-          <Typography style={{ margin: 10 }}>
-            The content of the Popper.
-          </Typography>
+          <MenuList>
+            {items.map((item, index) => (
+              <div key={index}>
+                <MenuItem onClick={handleClose}>{item} </MenuItem>
+              </div>
+            ))}
+          </MenuList>
         </Paper>
       </Popper>
     </div>
   );
 };
 
-export default SelectedMenu;
+export default BurgerMenu;
